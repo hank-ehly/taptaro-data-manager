3bfe2f389755f5b9f11df84a05ae7abf4fa25b18
Hank Ehly <hank.ehly@mulodo.com>
2016-01-02 11:05:39 -0600

    built

---
+++ app/models/category_item.rb
@@ -0,0 +1,13 @@
+class CategoryItem < ActiveRecord::Base
+  belongs_to :category
+
+  def self.to_csv(options = {})
+    CSV.generate(options) do |csv|
+      csv << column_names
+      all.each do |category_item|
+        csv << category_item.attributes.values_at(*column_names)
+      end
+    end
+  end
+
+end
