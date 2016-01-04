2674eb66b70d9693d0df353072778ff5c1525c49
Hank Ehly <hank.ehly@mulodo.com>
2016-01-03 19:23:13 -0600

    working csv output

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def export_csv(target=nil)
+  def export_csv()
+    target = params[:target]
     case target
       when 'categories'
         send_data Category.all.to_csv
       when 'category_items'
         send_data CategoryItem.all.to_csv
       else
-        render categories_path, danger: "Could not export to CSV"
+        flash[:danger] = "Could not export to CSV"
     end unless target.nil?
   end
 
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -23,7 +23,8 @@ class CategoriesController < ApplicationController
       flash[:success] = "Category was successfully created."
       redirect_to @category
     else
-      render :new, notice: @category.errors
+      flash[:danger] = @category.errors
+      render :new
     end
   end
 
@@ -32,7 +33,8 @@ class CategoriesController < ApplicationController
       flash[:success] = "Category was successfully updated."
       redirect_to @category
     else
-      render :edit, notice: @category.errors
+      flash[:danger] = @category.errors
+      render :edit
     end
   end
 
