7d9ed15bd40c1c8cdedab8c923d467691e276b93
hank-ehly <henry.ehly@gmail.com>
2016-01-03 20:42:25 -0600

    validations and devise

--- app/models/category_item.rb
+++ app/models/category_item.rb
@@ -1,5 +1,22 @@
+# == Schema Information
+#
+# Table name: category_items
+#
+#  id          :integer          not null, primary key
+#  romaji      :string
+#  kana        :string
+#  title       :string
+#  category_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class CategoryItem < ActiveRecord::Base
   belongs_to :category
+
+  validates :romaji, presence: true, length: {maximum: 25}
+  validates :kana, presence: true, length: {maximum: 25}
+  validates :title, presence: true, length: {maximum: 25}
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
