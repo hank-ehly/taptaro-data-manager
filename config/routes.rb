d083c651be33a14e037ecc97ea423ccc33c0709f
Hank Ehly <hank.ehly@mulodo.com>
2016-01-03 20:42:25 -0600

    validations and devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,41 @@
+# == Route Map
+#
+#                      Prefix Verb   URI Pattern                                                Controller#Action
+#                        root GET    /                                                          categories#index
+#           new_admin_session GET    /admins/sign_in(.:format)                                  devise/sessions#new
+#               admin_session POST   /admins/sign_in(.:format)                                  devise/sessions#create
+#       destroy_admin_session DELETE /admins/sign_out(.:format)                                 devise/sessions#destroy
+#              admin_password POST   /admins/password(.:format)                                 devise/passwords#create
+#          new_admin_password GET    /admins/password/new(.:format)                             devise/passwords#new
+#         edit_admin_password GET    /admins/password/edit(.:format)                            devise/passwords#edit
+#                             PATCH  /admins/password(.:format)                                 devise/passwords#update
+#                             PUT    /admins/password(.:format)                                 devise/passwords#update
+#   cancel_admin_registration GET    /admins/cancel(.:format)                                   devise/registrations#cancel
+#          admin_registration POST   /admins(.:format)                                          devise/registrations#create
+#      new_admin_registration GET    /admins/sign_up(.:format)                                  devise/registrations#new
+#     edit_admin_registration GET    /admins/edit(.:format)                                     devise/registrations#edit
+#                             PATCH  /admins(.:format)                                          devise/registrations#update
+#                             PUT    /admins(.:format)                                          devise/registrations#update
+#                             DELETE /admins(.:format)                                          devise/registrations#destroy
+#     category_category_items GET    /categories/:category_id/category_items(.:format)          category_items#index
+#                             POST   /categories/:category_id/category_items(.:format)          category_items#create
+#  new_category_category_item GET    /categories/:category_id/category_items/new(.:format)      category_items#new
+# edit_category_category_item GET    /categories/:category_id/category_items/:id/edit(.:format) category_items#edit
+#      category_category_item GET    /categories/:category_id/category_items/:id(.:format)      category_items#show
+#                             PATCH  /categories/:category_id/category_items/:id(.:format)      category_items#update
+#                             PUT    /categories/:category_id/category_items/:id(.:format)      category_items#update
+#                             DELETE /categories/:category_id/category_items/:id(.:format)      category_items#destroy
+#                  categories GET    /categories(.:format)                                      categories#index
+#                             POST   /categories(.:format)                                      categories#create
+#                new_category GET    /categories/new(.:format)                                  categories#new
+#               edit_category GET    /categories/:id/edit(.:format)                             categories#edit
+#                    category GET    /categories/:id(.:format)                                  categories#show
+#                             PATCH  /categories/:id(.:format)                                  categories#update
+#                             PUT    /categories/:id(.:format)                                  categories#update
+#                             DELETE /categories/:id(.:format)                                  categories#destroy
+#                      export GET    /export/:target(.:format)                                  application#export_csv
+#
+
 Rails.application.routes.draw do
 
   root 'categories#index'
