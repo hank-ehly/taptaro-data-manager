671c449313197f9f2e640e728c6014cbccdd6bb2
hank-ehly <henry.ehly@gmail.com>
2016-01-03 19:47:20 -0600

    export stuff

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,6 @@ class Category < ActiveRecord::Base
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
-      csv << column_names
       all.each do |category|
         csv << category.attributes.values_at(*column_names)
       end
