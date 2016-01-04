fbd849e1a3e2aabc5213ddf07ebf0929c9c72a58
Hank Ehly <hank.ehly@mulodo.com>
2016-01-03 18:39:17 -0600

    more stuff

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :category_items
   end
 
-  get 'csv' => 'categories#csv'
+  get 'export' => 'application#export_csv'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
