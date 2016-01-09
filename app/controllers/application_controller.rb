6303d4a0b57aaa6ca90902fd358dc516dbd610c9
hank-ehly <henry.ehly@gmail.com>
2016-01-09 23:34:43 +0900

    output feature complete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_admin!
+
+  include FileOutputHelper
+
+  def handle_file_output_request
+    output_latest_taptaro_files
+    redirect_to :back
+  end
+
 end
