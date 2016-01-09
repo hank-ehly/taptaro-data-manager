0dc9cae7037025916531f8821eeb8fbef80c361a
Hank Ehly <hank.ehly@mulodo.com>
2016-01-09 19:47:54 +0900

    refactor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,15 @@
 module ApplicationHelper
 
-  def self.infer_alert_class_by_flash_key(key='')
+  # @param [String] key
+  # @return [String]
+  def infer_alert_class_by_flash_key(key='')
     key == 'notice' || key == 'alert' ? 'info' : key
+  end
+
+  # @param ActiveRecord object
+  # @return :partial
+  def display_errors_for_object(object)
+    render partial: 'common/error_message', locals: { :object => object }
   end
 
 end
