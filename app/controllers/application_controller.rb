5ffd0d423117cbd171b267e52af8628483e4776d
hank-ehly <henry.ehly@gmail.com>
2016-01-09 09:36:51 +0900

    remove export functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :authenticate_admin!
-
-  def export_csv()
-    target = params[:target]
-
-    case target
-      when 'categories'
-        send_data Category.all.to_csv
-      when 'category_items'
-        send_data CategoryItem.all.to_csv
-      else
-        flash[:danger] = "Could not export to CSV"
-    end unless target.nil?
-
-  end
-
 end
