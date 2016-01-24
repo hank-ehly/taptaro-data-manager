296b8ccd1b7d5f0adf32453a1203c5870f471a68
hank-ehly <henry.ehly@gmail.com>
2016-01-24 17:55:55 +0900

    slow audio clip functionality

--- app/controllers/category_items_controller.rb
+++ app/controllers/category_items_controller.rb
@@ -53,7 +53,14 @@ class CategoryItemsController < ApplicationController
   end
 
   def category_item_params
-    params.require(:category_item).permit(:romaji, :kana, :title, :category_id, :female_audio_clip, :male_audio_clip)
+    params.require(:category_item).permit(:romaji,
+                                          :kana,
+                                          :title,
+                                          :category_id,
+                                          :female_audio_clip,
+                                          :slow_female_audio_clip,
+                                          :male_audio_clip,
+                                          :slow_male_audio_clip)
   end
 
 end
--- app/models/category_item.rb
+++ app/models/category_item.rb
@@ -17,6 +17,8 @@ class CategoryItem < ActiveRecord::Base
 
   mount_uploader :female_audio_clip, FemaleAudioClipUploader
   mount_uploader :male_audio_clip, MaleAudioClipUploader
+  mount_uploader :slow_female_audio_clip, SlowFemaleAudioClipUploader
+  mount_uploader :slow_male_audio_clip, SlowMaleAudioClipUploader
 
   validates :romaji, presence: true, length: { maximum: 25 }
   validates :kana, presence: true, length: { maximum: 25 }
@@ -29,6 +31,25 @@ class CategoryItem < ActiveRecord::Base
         csv << category_item.attributes.values_at(*column_names)
       end
     end
+  end
+
+
+  def self.get_play_icon_id_by_audio_clip(audio_clip={})
+    get_id_by_type_for_audio_clip('play_icon', audio_clip)
+  end
+
+  def self.get_audio_clip_id_by_audio_clip(audio_clip={})
+    get_id_by_type_for_audio_clip('audio_clip', audio_clip)
+  end
+
+
+  private
+
+  def self.get_id_by_type_for_audio_clip(type='', audio_clip={})
+    id = ''
+    id += 'slow_' if audio_clip[:is_slow]
+    id += audio_clip[:gender] + '_' + type
+    id
   end
 
 end
