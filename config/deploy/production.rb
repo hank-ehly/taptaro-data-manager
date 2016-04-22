be2a67b335226ea1e0e9f5079a96362b7c6cf53a
hank-ehly <henry.ehly@gmail.com>
2016-04-23 08:56:38 +0900

    Yet another auth change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -54,8 +54,8 @@ server ENV['SAKURAIP'],
   roles: %w{web app},
   ssh_options: {
     # user: 'user_name', # overrides user setting above
-    # keys: %w(/Users/henryehly/.ssh/sakura/id_rsa_sakura.pub),
+    keys: %w(/Users/henryehly/.ssh/sakura/id_rsa_sakura),
     # forward_agent: false,
-    # auth_methods: %w(publickey)
-    password: ENV['SAKURAPASSWORD']
+    auth_methods: %w(publickey)
+    # password: ENV['SAKURAPASSWORD']
   }
