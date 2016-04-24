7fa9f79c16d6d9b883bfd7ba6e785eb9328c1ec7
hank-ehly <henry.ehly@gmail.com>
2016-04-24 12:42:04 +0900

    Return empty body

--- app/controllers/user_messages_controller.rb
+++ app/controllers/user_messages_controller.rb
@@ -1,17 +1,15 @@
 class UserMessagesController < ApplicationController
 
   def create
-    success = false
 
     if params[:message]
       user_message = UserMessage.new(message: params[:message])
       if user_message.save!
         UserMessageMailer.taptaro_notification_email(user_message).deliver
-        success = true
       end
     end
 
-    render json: {result: success.to_s}
+    render :nothing => true
 
   end
 end
